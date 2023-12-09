66517e1e6eb883a92f5ac1535b1e727715d08c33
Vu Hoang Phuc <vuhoangphuc11@gmail.com>
2023-12-09 11:18:27 +0700

    add more package

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
-from routes import router as book_router
+from routers.books import router as book_router
 
 config = dotenv_values(".env")
 
